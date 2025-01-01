9d390f0bf6b52b61adb39a21589f2cced247b346
Md. Montasir Alam <166697994+montasir132@users.noreply.github.com>
2025-01-01 07:47:34 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ if(isset($_POST['addition'])){
       <div class="row">
         <div class="col-lg-10">
           <!-- form start  -->
-          <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+          <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="needs-validation" novalidate>
             <div class="mb-3 mt-3">
               <label for="1st_number" class="form-label">1st Number</label>
               <input type="number" class="form-control 1st_number" id="1st_number" name="1st_number" placeholder="Enter your first number" required>
@@ -60,17 +60,21 @@ if(isset($_POST['addition'])){
             <button type="submit" class="btn btn-danger" name="subtraction">(-)subtraction</button>
             <button type="submit" class="btn btn-warning" name="multiply">(*)multiply</button>
             <button type="submit" class="btn btn-primary" name="divide">(/)divide</button>
+            <script>
+              
+            </script>                                            
           </form>
           <!-- form end  -->
         </div>
       </div>
     </div>
-    <div class="container mt-5">
+    <div class="container mt-4">
       <?php 
-      echo "<H1>".$number3."</H1>";
+      echo "<H1>"."Result=".$number3."</H1>";
       ?>
     </div>
     <!--js links-->
     <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/scrip.js"></script>
   </body>
 </html>
